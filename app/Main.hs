3fa891837511dbaf09e674e1c4f373a7781c7bc5
Matt Hunzinger <matthunz2@gmail.com>
2025-04-03 19:30:29 -0400

    feat: Config type

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import MyLib
 
 main :: IO ()
-main = run
+main = run defaultConfig
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -2,18 +2,79 @@
 
 module MyLib where
 
+import Control.Concurrent
+import Control.Monad.State.Strict
+import Data.Word
 import SDL
 
-run :: IO ()
-run = do
-  initializeAll
-  window <- createWindow "Fisticuffs" defaultWindow
-  renderer <- createRenderer window (-1) defaultRenderer
-  draw renderer
+data RenderType = UnacceleratedRender | AcceleratedRender | SoftwareRender
+  deriving (Show, Eq)
 
-draw :: Renderer -> IO ()
-draw renderer = do
-  rendererDrawColor renderer $= V4 255 0 0 255
-  fillRect renderer (Just (Rectangle (P (V2 10 10)) (V2 50 50)))
-  present renderer
-  draw renderer
+data VSyncType = VSync | NoVSync RenderType Word32
+  deriving (Show, Eq)
+
+data Config = Config
+  { configWindowSize :: V2 Int,
+    configVSync :: VSyncType
+  }
+
+defaultConfig :: Config
+defaultConfig =
+  Config
+    { configWindowSize = V2 800 600,
+      configVSync = VSync
+    }
+
+newGame :: Config -> IO GameState
+newGame config = do
+  initializeAll
+  let size = configWindowSize config
+      vsyncType = configVSync config
+      windowConfig =
+        defaultWindow
+          { windowInitialSize = fmap fromIntegral size,
+            windowResizable = True
+          }
+      ty = case vsyncType of
+        VSync -> AcceleratedVSyncRenderer
+        NoVSync renderType _ -> case renderType of
+          UnacceleratedRender -> SoftwareRenderer
+          AcceleratedRender -> AcceleratedRenderer
+          SoftwareRender -> SoftwareRenderer
+  window <- createWindow "Fisticuffs" windowConfig
+  renderer <- createRenderer window (-1) (defaultRenderer {rendererType = ty})
+  return GameState {gameWindow = window, gameRenderer = renderer, gameVSync = vsyncType}
+
+data GameState = GameState
+  { gameWindow :: Window,
+    gameRenderer :: Renderer,
+    gameVSync :: VSyncType
+  }
+
+draw :: GameState -> IO ()
+draw game = do
+  case gameVSync game of
+    VSync -> do
+      rendererDrawColor (gameRenderer game) $= V4 255 0 0 255
+      fillRect (gameRenderer game) (Just (Rectangle (P (V2 10 10)) (V2 50 50)))
+      present $ gameRenderer game
+
+      draw game
+    NoVSync _ frameDelayUs -> do
+      startTicks <- ticks
+
+      rendererDrawColor (gameRenderer game) $= V4 255 0 0 255
+      fillRect (gameRenderer game) (Just (Rectangle (P (V2 10 10)) (V2 50 50)))
+      present $ gameRenderer game
+
+      endTicks <- ticks
+      let elapsedMs = endTicks - startTicks
+          elapsedUs = fromIntegral elapsedMs * 1000
+          delayTime = frameDelayUs - elapsedUs
+      when (delayTime > 0) . threadDelay $ fromIntegral delayTime
+      draw game
+
+run :: Config -> IO ()
+run cfg = do
+  gameState <- newGame cfg
+  draw gameState
