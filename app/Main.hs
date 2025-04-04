8ec7aeae34c4fbf5c5367eb2249e081b5c324582
Matt Hunzinger <matthunz2@gmail.com>
2025-04-03 23:37:34 -0400

    feat: messy physics

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,14 @@
 module Main where
 
+import Linear
 import MyLib
 
 main :: IO ()
 main = run defaultConfig $ do
   e <- addCharacter
   animate e "run"
+  face e West
+
+  jump e
+
   return ()
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module MyLib where
 
@@ -26,7 +27,7 @@ data Config = Config
 defaultConfig :: Config
 defaultConfig =
   Config
-    { configWindowSize = V2 800 600,
+    { configWindowSize = V2 2000 1000,
       configVSync = VSync,
       configCharacterTexture = "assets/character.png"
     }
@@ -44,18 +45,22 @@ data Direction = East | West
 
 data Character = Character
   { characterPosition :: V2 Int,
+    characterVelocity :: V2 Float,
+    characterAcceleration :: V2 Float,
     characterDirection :: Direction,
     characterSize :: V2 Int,
     characterTexture :: Texture,
     characterAnimations :: Map String Animation,
     characterAnimation :: String,
+    characterActionAnimation :: Maybe (String, Word32),
     characterAnimationStart :: Word32
   }
 
 data GameState = GameState
   { gameWindow :: Window,
     gameRenderer :: Renderer,
-    gameTime :: Word32,
+    gameTick :: Word32,
+    gameLastTick :: Word32,
     gameVSync :: VSyncType,
     gameCharacterTexture :: Texture,
     gameCharacters :: Map EntityID Character,
@@ -87,68 +92,114 @@ newGame config = do
       { gameWindow = window,
         gameRenderer = renderer,
         gameVSync = vsyncType,
-        gameTime = t,
+        gameTick = t,
+        gameLastTick = t,
         gameCharacterTexture = texture,
         gameCharacters = Map.empty,
         gameNextEntityID = EntityID 0
       }
 
-draw :: GameState -> IO ()
-draw game = do
+draw :: Game ()
+draw = do
   start <- ticks
-  let game' = game {gameTime = start}
+  game <- Game get
+  let game' = game {gameTick = start, gameLastTick = gameTick game}
+  Game $ put game'
   case gameVSync game' of
     VSync -> do
       rendererDrawColor (gameRenderer game') $= V4 0 0 0 255
       clear $ gameRenderer game'
-      drawCharacters game'
+      drawCharacters
       present $ gameRenderer game'
 
-      draw game'
+      draw
     NoVSync _ frameDelayUs -> do
       rendererDrawColor (gameRenderer game') $= V4 0 0 0 255
       clear $ gameRenderer game'
-      drawCharacters game'
+      drawCharacters
       present $ gameRenderer game'
 
       endTicks <- ticks
       let elapsedMs = endTicks - start
           elapsedUs = fromIntegral elapsedMs * 1000
           delayTime = frameDelayUs - elapsedUs
-      when (delayTime > 0) . threadDelay $ fromIntegral delayTime
-      draw game'
+      liftIO $ when (delayTime > 0) . threadDelay $ fromIntegral delayTime
+      draw
 
-drawCharacters :: GameState -> IO ()
-drawCharacters game = do
+drawCharacters :: Game ()
+drawCharacters = Game $ do
+  game <- get
   let characters = gameCharacters game
-  forM_ (Map.toList characters) $ \(EntityID _, character) -> do
-    let pos = fromIntegral <$> characterPosition character
-        a = characterAnimations character Map.! characterAnimation character
-        col =
-          fromIntegral (gameTime game - characterAnimationStart character)
-            `div` 100
-            `mod` animationCols a
-        V2 width height = characterSize character
-        flipped = case characterDirection character of
-          East -> False
-          West -> True
-    copyEx
-      (gameRenderer game)
-      (characterTexture character)
-      ( Just
-          ( Rectangle
-              ( P $
-                  V2
-                    (fromIntegral $ col * width)
-                    (fromIntegral $ height * animationRow a)
-              )
-              (V2 (fromIntegral width) (fromIntegral height))
-          )
+  characters' <-
+    traverse
+      ( \character -> do
+          let character' = updateCharacter game character
+              pos = fromIntegral <$> characterPosition character'
+              (row, col, character'') = case characterActionAnimation character' of
+                Just (s, duration) ->
+                  let a = characterAnimations character' Map.! s
+                      c =
+                        fromIntegral (gameTick game - characterAnimationStart character')
+                          `div` 100
+                   in if c > duration
+                        then
+                          let a' = characterAnimations character' Map.! characterAnimation character'
+                              c' =
+                                fromIntegral (gameTick game - characterAnimationStart character')
+                                  `div` 100
+                                  `mod` animationCols a'
+                           in ( animationRow a',
+                                c',
+                                character'
+                                  { characterActionAnimation = Nothing,
+                                    characterAnimationStart = gameTick game
+                                  }
+                              )
+                        else (animationRow a, fromIntegral c, character')
+                Nothing ->
+                  let a = characterAnimations character' Map.! characterAnimation character'
+                      c =
+                        fromIntegral (gameTick game - characterAnimationStart character')
+                          `div` 100
+                          `mod` animationCols a
+                   in (animationRow a, c, character')
+              V2 width height = characterSize character''
+              flipped = case characterDirection character'' of
+                East -> False
+                West -> True
+          copyEx
+            (gameRenderer game)
+            (characterTexture character'')
+            ( Just
+                ( Rectangle
+                    ( P $
+                        V2
+                          (fromIntegral $ col * width)
+                          (fromIntegral $ height * row)
+                    )
+                    (V2 (fromIntegral width) (fromIntegral height))
+                )
+            )
+            (Just (Rectangle (P pos) (V2 1000 1000)))
+            1
+            Nothing
+            (V2 flipped False)
+          return character''
       )
-      (Just (Rectangle (P pos) (V2 1000 1000)))
-      1
-      Nothing
-      (V2 flipped False)
+      characters
+  put $ game {gameCharacters = characters'}
+
+updateCharacter :: GameState -> Character -> Character
+updateCharacter game character =
+  let dt = fromIntegral $ gameTick game - gameLastTick game
+      a = characterAcceleration character + V2 0 ((9.81 / 50000) * dt)
+      v = characterVelocity character + a * V2 dt dt
+      newPos = (fromIntegral <$> characterPosition character) + (v * V2 dt dt)
+   in character
+        { characterPosition = fmap (round . min 0) newPos, -- TODO
+          characterVelocity = v,
+          characterAcceleration = a
+        }
 
 newtype Game a = Game {unGame :: StateT GameState IO a}
   deriving (Functor, Applicative, Monad, MonadIO)
@@ -160,16 +211,19 @@ addCharacter :: Game EntityID
 addCharacter = Game $ do
   gameState <- get
   let EntityID e = gameNextEntityID gameState
-      animations = Map.fromList [("idle", Animation 0 8), ("run", Animation 1 8)]
+      animations = Map.fromList [("idle", Animation 0 8), ("run", Animation 1 8), ("jump", Animation 2 2)]
       newCharacter =
         Character
           { characterPosition = V2 0 0,
-            characterDirection = West,
+            characterVelocity = V2 0 0,
+            characterAcceleration = V2 0 0,
+            characterDirection = East,
             characterSize = V2 200 200,
             characterTexture = gameCharacterTexture gameState,
             characterAnimations = animations,
             characterAnimation = "idle",
-            characterAnimationStart = gameTime gameState
+            characterActionAnimation = Nothing,
+            characterAnimationStart = gameTick gameState
           }
       newCharacters = Map.insert (EntityID e) newCharacter (gameCharacters gameState)
       newGameState =
@@ -189,16 +243,72 @@ animate entityID animationName = Game $ do
       let newCharacter =
             character
               { characterAnimation = animationName,
-                characterAnimationStart = gameTime gameState
+                characterAnimationStart = gameTick gameState
               }
           newCharacters = Map.insert entityID newCharacter characters
           newGameState = gameState {gameCharacters = newCharacters}
        in put newGameState
     Nothing -> liftIO $ putStrLn $ "EntityID " ++ show entityID ++ " not found."
 
+animateAction :: EntityID -> String -> Word32 -> Game ()
+animateAction entityID animationName duration = Game $ do
+  gameState <- get
+  let characters = gameCharacters gameState
+  case Map.lookup entityID characters of
+    Just character ->
+      let newCharacter =
+            character
+              { characterActionAnimation = Just (animationName, duration),
+                characterAnimationStart = gameTick gameState
+              }
+          newCharacters = Map.insert entityID newCharacter characters
+          newGameState = gameState {gameCharacters = newCharacters}
+       in put newGameState
+    Nothing -> liftIO $ putStrLn $ "EntityID " ++ show entityID ++ " not found."
+
+face :: EntityID -> Direction -> Game ()
+face entityID direction = Game $ do
+  gameState <- get
+  let characters = gameCharacters gameState
+  case Map.lookup entityID characters of
+    Just character ->
+      let newCharacter =
+            character
+              { characterDirection = direction,
+                characterAnimationStart = gameTick gameState
+              }
+          newCharacters = Map.insert entityID newCharacter characters
+          newGameState = gameState {gameCharacters = newCharacters}
+       in put newGameState
+    Nothing -> liftIO $ putStrLn $ "EntityID " ++ show entityID ++ " not found."
+
+accelerate :: EntityID -> V2 Float -> Game ()
+accelerate entityID acceleration = Game $ do
+  gameState <- get
+  let characters = gameCharacters gameState
+  case Map.lookup entityID characters of
+    Just character ->
+      let newCharacter =
+            character
+              { characterVelocity = acceleration,
+                characterAcceleration = acceleration
+              }
+          newCharacters = Map.insert entityID newCharacter characters
+          newGameState = gameState {gameCharacters = newCharacters}
+       in put newGameState
+    Nothing -> liftIO $ putStrLn $ "EntityID " ++ show entityID ++ " not found."
+
+jump :: EntityID -> Game ()
+jump e = do
+  animateAction e "jump" 2
+  accelerate e (V2 0 (-0.03))
+
 run :: Config -> Game a -> IO a
 run cfg game = do
   gameState <- newGame cfg
-  (a, gameState') <- runGameT game gameState
-  draw gameState'
+  let game' = do
+        a <- game
+        draw
+        return a
+  (a, _) <- runGameT game' gameState
   return a
